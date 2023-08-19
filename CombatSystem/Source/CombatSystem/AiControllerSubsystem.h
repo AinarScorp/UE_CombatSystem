2edf7e35c53d5175d3a6a000aa851f39ce410c24
AinarScorpLaptop <Einarhallik@gmail.com>
2023-08-19 18:00:01 +0200

    DOne a lot of AI stuff

---
+++ CombatSystem/Source/CombatSystem/AiControllerSubsystem.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "AiControllerSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBATSYSTEM_API UAiControllerSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+private:
+	UPROPERTY()
+	TArray<AActor*> ActiveEnemies;
+public:
+	UFUNCTION(BlueprintPure, BlueprintCallable)
+	TArray<AActor*> GetActiveEnemies() { return ActiveEnemies;}
+	UFUNCTION(BlueprintCallable)
+	void AddToActiveEnemies(AActor* NewActiveEnemy) {ActiveEnemies.AddUnique(NewActiveEnemy);}
+	UFUNCTION(BlueprintCallable)
+	void RemoveActiveEnemies(AActor* ActiveEnemyToRemove) {ActiveEnemies.RemoveSingle(ActiveEnemyToRemove);}
+	
+	
+};
