9c9e88c53b7beff896888f6b5383dcee7184f56e
AinarScorp <Einarhallik@gmail.com>
2023-11-14 17:25:43 +0100

    Renaming files

---
+++ CombatSystem/Source/CombatSystem/Public/Characters/CombatSystem_PlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/CombatSystem_CharacterBase.h"
+#include "CombatSystem_PlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBATSYSTEM_API ACombatSystem_PlayerCharacter : public ACombatSystem_CharacterBase
+{
+	GENERATED_BODY()
+	
+};
